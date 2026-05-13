f61b6c0e6a68a72960cfebc6da928677cdac5369
Efe <efefatih04042007@gmail.com>
2026-05-13 17:39:23 +0300

    globalvars

--- shii_vars.h
+++ shii_vars.h
@@ -2,8 +2,8 @@
 
 class c_context {
 public:
-  void* original_sys_fn    = 0; // Eski adıyla: get_system_info_hook_trp
-  void* original_module_fn = 0; // Eski adıyla: dll_main_hook_trp
+  void* original_sys_fn    = 0; 
+  void* original_module_fn = 0; 
 };
 
 CLASS_EXTERN(c_context, ctx);
